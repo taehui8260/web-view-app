9800569bd4670ea7bbf546715c1e50ab91e019d7
lth1178 <lth1178@gmail.com>
2024-03-15 00:32:28 +0900

    ADD: 최초커밋

---
+++ app/src/main/java/com/taehui/appearance_man/MainActivity.java
@@ -0,0 +1,210 @@
+package com.taehui.appearance_man;
+import com.taehui.appearance_man.BuildConfig;
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private ValueCallback<Uri[]> filePathCallback;
+    private Uri photoURI = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        // JavaScript 인터페이스 추가
+        webView.addJavascriptInterface(new WebAppInterface(this), "Android");
+        webView.loadUrl(BuildConfig.WEB_URL);
+        checkVerify();
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // 여기에서 url을 확인하고, 특정 조건일 때만 view.loadUrl(url);을 호출합니다.
+                // 그 외의 경우는 현재 웹뷰에서 로딩하도록 false를 반환합니다.]
+                if (url.contains("netlify.app")) {
+                    // 현재 웹뷰에서 URL을 로드합니다.
+                    view.loadUrl(url);
+                    return true; // 현재 웹뷰에서 처리되었음을 나타냅니다.
+                } else {
+                    // netlify.app이 포함되지 않은 URL은 새 창에서 열도록 합니다.
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    view.getContext().startActivity(intent);
+                    return true; // 이 메서드에서 처리되었음을 나타냅니다.
+                }
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathsCallback, FileChooserParams fileChooserParams) {
+                if (filePathCallback != null) {
+                    filePathCallback.onReceiveValue(null);
+                }
+                Log.v("### onShowFileChooser", "onShowFileChooser");
+
+                filePathCallback = filePathsCallback;
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+                    Log.v("###This device does not have a camera.)", "");
+
+                }
+                Log.v("### getPackageManager", String.valueOf(takePictureIntent.resolveActivity(getPackageManager())));
+                //if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                // Create the File where the photo should go
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                    Log.v("### photoFile", String.valueOf(photoFile));
+                    if (photoFile != null) {
+                        photoURI = FileProvider.getUriForFile(MainActivity.this, getApplicationContext().getPackageName() + ".provider", photoFile);
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    }
+                } catch (IOException ex) {
+                    Log.e("### photoFile", "error", ex);
+                    // Error occurred while creating the File
+                }
+                //}
+
+                Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                contentSelectionIntent.setType("image/*");
+                Log.v("### contentSelectionIntent", String.valueOf(contentSelectionIntent));
+
+                Intent[] intentArray;
+                if (takePictureIntent != null) {
+                    intentArray = new Intent[]{takePictureIntent};
+                } else {
+                    intentArray = new Intent[0];
+                }
+
+                Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+                chooserIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+
+                someActivityResultLauncher.launch(chooserIntent);
+                return true;
+            }
+        });
+    }
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+        Log.v("### image", String.valueOf(image));
+
+        return image;
+    }
+
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Log.v("### data1", String.valueOf(result));
+
+                    if (result.getResultCode() == AppCompatActivity.RESULT_OK) {
+                        Intent data = result.getData();
+                        Uri[] results = null;
+
+                        // 카메라에서 사진을 찍고 결과로 Uri가 제공되지 않는 경우, createImageFile()에서 생성한 파일 위치를 사용
+                        if (data == null || data.getData() == null) {
+                            if (photoURI != null) { // cameraPhotoPath는 사진 파일의 경로
+                                results = new Uri[]{photoURI};
+                            }
+                        } else {
+                            String dataString = data.getDataString();
+                            if (dataString != null) {
+                                results = new Uri[]{Uri.parse(dataString)};
+                            }
+                        }
+
+                        if (filePathCallback != null) {
+                            filePathCallback.onReceiveValue(results);
+                            filePathCallback = null;
+                        }
+                    } else {
+                        if (filePathCallback != null) {
+                            filePathCallback.onReceiveValue(null);
+                            filePathCallback = null;
+                        }
+                    }
+                }
+            });
+
+    public class WebAppInterface {
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        /** Show a toast from the web page */
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
+        // 여기에 카메라를 여는 메소드를 추가할 수 있습니다.
+    }
+
+    public void checkVerify() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+//            Log.d("checkVerify() : ","if문 들어옴");
+
+            //카메라 또는 저장공간 권한 획득 여부 확인
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+
+                Toast.makeText(getApplicationContext(), "권한 관련 요청을 허용해 주셔야 카메라 캡처이미지 사용등의 서비스를 이용가능합니다.", Toast.LENGTH_SHORT).show();
+
+            } else {
+//                Log.d("checkVerify() : ","카메라 및 저장공간 권한 요청");
+                // 카메라 및 저장공간 권한 요청
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET, Manifest.permission.CAMERA,
+                        Manifest.permission.ACCESS_NETWORK_STATE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            }
+        }
+    }
+
+}
